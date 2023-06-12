c2ff990211165515f76ac38043bb22f22c305ea1
xuwen <996592197@qq.com>
2023-06-12 16:25:48 +0800

    toast

--- FFCommon/FFCommon/Extension/Tool/Toast.swift
+++ FFCommon/FFCommon/Extension/Tool/Toast.swift
@@ -6,41 +6,41 @@
 //
 
 import UIKit
-import Toast_Swift
-
-public class Toast{
-
-    public static func config() -> ToastStyle{
-        var style = ToastStyle() //样式
-        style.messageFont = .small
-        style.titleFont = .normal
-        //        style.backgroundColor = .white
-        //        style.titleColor = .black
-        //        style.messageColor = .black
-        return style
-    }
-
-    public static func show(_ titie:String? ,view:UIView? = UIApplication.keyWindow){
-        if let v = view{
-            v.makeToast( titie, point: CGPointMake(v.width/2, v.height/2), title: nil, image: nil,style:Toast.config(),completion: { didTap in
-
-            })
-        }else{
-            UIApplication.keyWindow?.makeToast( titie, point: CGPointMake(kScreenWidth/2,kScreenHeight/2), title: nil, image: nil,style:Toast.config(),completion: { didTap in
-
-            })
-        }
-
-    }
-}
-
-public class Loading{
-    public static func show(){
-        UIApplication.keyWindow?.makeToastActivity(.center)
-    }
-    public static func hidden(){
-        UIApplication.keyWindow?.hideToastActivity()
-    }
-}
-
+//import Toast_Swift
+//
+//public class Toast{
+//
+//    public static func config() -> ToastStyle{
+//        var style = ToastStyle() //样式
+//        style.messageFont = .small
+//        style.titleFont = .normal
+//        //        style.backgroundColor = .white
+//        //        style.titleColor = .black
+//        //        style.messageColor = .black
+//        return style
+//    }
+//
+//    public static func show(_ titie:String? ,view:UIView? = UIApplication.keyWindow){
+//        if let v = view{
+//            v.makeToast( titie, point: CGPointMake(v.width/2, v.height/2), title: nil, image: nil,style:Toast.config(),completion: { didTap in
+//
+//            })
+//        }else{
+//            UIApplication.keyWindow?.makeToast( titie, point: CGPointMake(kScreenWidth/2,kScreenHeight/2), title: nil, image: nil,style:Toast.config(),completion: { didTap in
+//
+//            })
+//        }
+//
+//    }
+//}
+//
+//public class Loading{
+//    public static func show(){
+//        UIApplication.keyWindow?.makeToastActivity(.center)
+//    }
+//    public static func hidden(){
+//        UIApplication.keyWindow?.hideToastActivity()
+//    }
+//}
+//
 
